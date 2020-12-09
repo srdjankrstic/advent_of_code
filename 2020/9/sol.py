fd47747c8eab3b79c9700044ef557bd70d61d4fa
Srdjan Krstic <srdjan@cedar.com>
2020-12-09 00:12:46 -0500

    day 9

--- 2020/9/sol.py
+++ 2020/9/sol.py
@@ -7,16 +7,42 @@ import math
 
 def solve1(lines):
     sol = 0
+    nums = []
+    pre = 5 if args.part == 1 else 25
+    n = 0
     for line in lines.splitlines():
-        pass
-    return sol
+        if not line:
+            break
+        nums.append(int(line))
+    n = len(nums)
+    for i in range(pre, n):
+        ok = False
+        for j in range(pre):
+            for k in range(pre):
+                if j == k:
+                    continue
+                if nums[i - j - 1] + nums[i - k - 1] == nums[i]:
+                    ok = True
+                    break
+        if not ok:
+            return nums[i]
 
 
 def solve2(lines):
-    sol = 0
+    sol1 = solve1(lines)
+    nums = []
     for line in lines.splitlines():
-        pass
-    return sol
+        if not line:
+            break
+        nums.append(int(line))
+    n = len(nums)
+    sums = [0] * n
+    sums[0] = nums[0]
+    for i in range(1, n):
+        sums[i] = sums[i - 1] + nums[i]
+        for j in range(i):
+            if sums[i] - sums[j] == sol1:
+                return min(nums[j+1:i+1]) + max(nums[j+1:i+1])
 
 
 parser = argparse.ArgumentParser()
