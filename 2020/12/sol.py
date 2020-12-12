40b357cdf5f5c30fd816b341bef7d16a39b3a27b
Srdjan Krstic <krstic.m.srdjan@gmail.com>
2020-12-12 00:44:08 -0500

    tabs vs spaces

--- 2020/12/sol.py
+++ 2020/12/sol.py
@@ -20,72 +20,72 @@ xx = 0
 yy = 0
 
 if args.part == 1:
-	for line in lines.splitlines():
-		if not line:
-			continue
-		m = re.match("^(.)(.*)$", line)
-		c = m.group(1)
-		v = int(m.group(2))
-		if c == 'N':
-			yy += v
-		elif c == 'S':
-			yy -= v
-		elif c == 'E':
-			xx += v
-		elif c == 'W':
-			xx -= v
-		elif c == 'L':
-			d = int((d + v/90 + 4) % 4)
-		elif c == 'R':
-			d = int((d - v/90 + 4) % 4)
-		elif c == 'F':
-			xx += di[d] * v
-			yy += dj[d] * v
-		print(abs(xx) + abs(yy))
+    for line in lines.splitlines():
+        if not line:
+            continue
+        m = re.match("^(.)(.*)$", line)
+        c = m.group(1)
+        v = int(m.group(2))
+        if c == 'N':
+            yy += v
+        elif c == 'S':
+            yy -= v
+        elif c == 'E':
+            xx += v
+        elif c == 'W':
+            xx -= v
+        elif c == 'L':
+            d = int((d + v/90 + 4) % 4)
+        elif c == 'R':
+            d = int((d - v/90 + 4) % 4)
+        elif c == 'F':
+            xx += di[d] * v
+            yy += dj[d] * v
+        print(abs(xx) + abs(yy))
 
 if args.part == 2:
-	wx = 10
-	wy = 1
-	for line in lines.splitlines():
-		if not line:
-			continue
-		m = re.match("^(.)(.*)$", line)
-		c = m.group(1)
-		v = int(m.group(2))
-		if c == 'N':
-			wy += v
-		elif c == 'S':
-			wy -= v
-		elif c == 'E':
-			wx += v
-		elif c == 'W':
-			wx -= v
-		elif c == 'L':
-			if v == 90:
-				t = wx
-				wx = -wy
-				wy = t
-			elif v == 180:
-				wx *= -1
-				wy *= -1
-			elif v == 270:
-				t = wx
-				wx = wy
-				wy = -t
-		elif c == 'R':
-			if v == 90:
-				t = wx
-				wx = wy
-				wy = -t
-			elif v == 180:
-				wx *= -1
-				wy *= -1
-			elif v == 270:
-				t = wx
-				wx = -wy
-				wy = t
-		elif c == 'F':
-			xx += wx * v
-			yy += wy * v
-#		print(f"{xx}, {yy}, {wx}, {wy}")
-	print(abs(xx) + abs(yy))
+    wx = 10
+    wy = 1
+    for line in lines.splitlines():
+        if not line:
+            continue
+        m = re.match("^(.)(.*)$", line)
+        c = m.group(1)
+        v = int(m.group(2))
+        if c == 'N':
+            wy += v
+        elif c == 'S':
+            wy -= v
+        elif c == 'E':
+            wx += v
+        elif c == 'W':
+            wx -= v
+        elif c == 'L':
+            if v == 90:
+                t = wx
+                wx = -wy
+                wy = t
+            elif v == 180:
+                wx *= -1
+                wy *= -1
+            elif v == 270:
+                t = wx
+                wx = wy
+                wy = -t
+        elif c == 'R':
+            if v == 90:
+                t = wx
+                wx = wy
+                wy = -t
+            elif v == 180:
+                wx *= -1
+                wy *= -1
+            elif v == 270:
+                t = wx
+                wx = -wy
+                wy = t
+        elif c == 'F':
+            xx += wx * v
+            yy += wy * v
+#        print(f"{xx}, {yy}, {wx}, {wy}")
+    print(abs(xx) + abs(yy))
